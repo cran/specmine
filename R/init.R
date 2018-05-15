e897ecd803112226fc6777c18401250d8e43d796
Miguel Rocha <mrocha@di.uminho.pt>
2018-05-15 15:54:00 +0000

    version 2.0.3

--- R/init.R
+++ R/init.R
@@ -3,19 +3,28 @@ globalVariables("group")
 names = c("genefilter", "xcms", "impute", "MAIT")
 
 for (n in 1:length(names)){
-	if (!(names[n] %in% rownames(installed.packages()))){
-		source("http://bioconductor.org/biocLite.R")
-		biocLite(names[n])
-	}
+  if (!(names[n] %in% rownames(installed.packages()))){
+    if (interactive()) {
+      repeat {
+        cat(paste("Install necessary package ", names[n], "from bioconductor repository? [y or n]"))
+        answer <- readLines(n = 1)
+        if (answer %in% c("y", "Y", "n", "N")){
+          source("http://bioconductor.org/biocLite.R")
+          biocLite(names[n])
+        }
+      }
+    }
+  }
 }
 
-if (!("rcytoscapejs" %in% rownames(installed.packages()))){
-	#if (!("devtools" %in% rownames(installed.packages()))) {
-	#  if (!("base64enc" %in% rownames(installed.packages()))) install.packages("base64enc", repos="https://cloud.r-project.org")
-	#  if (!("memoise" %in% rownames(installed.packages()))) install.packages("memoise", repos="https://cloud.r-project.org")#, dependencies=TRUE)
-	#  install.packages("devtools", repos="https://cloud.r-project.org")#, dependencies=TRUE)
-	#}
-	install_github('cytoscape/r-cytoscape.js')
+if (interactive()) {
+  repeat {
+    cat(paste("Install necessary package rcytoscapejs from github? [y or n]"))
+    answer <- readLines(n = 1)
+    if (answer %in% c("y", "Y", "n", "N")){
+      install_github('cytoscape/r-cytoscape.js')
+    }
+  }
 }
 
 #if (!("mzmatch.R" %in% rownames(installed.packages()))){
